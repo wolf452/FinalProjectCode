37dddb4c3b19d9d848702e79fb420048cfeac72d
ahmedmaher <121304760+wolf452@users.noreply.github.com>
2024-12-25 23:54:51 +0200

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,11 +1,7 @@
 output "instance_id" {
-  value = aws_instance.ec2.id
+  value = module.ec2_instance.instance_id
 }
 
 output "ec2_public_ips" {
-  value = aws_instance.ec2.public_ip
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.ec2.public_ip
+  value = module.ec2_instance.public_ip
 }
