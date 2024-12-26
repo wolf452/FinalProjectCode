5172c0b7e8be8f847a34c283acbbbdec06a0bf4a
ahmedmaher <121304760+wolf452@users.noreply.github.com>
2024-12-27 00:35:15 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "jenk-buckett"
+    bucket         = "bucket-last1"
     key            = "terraform/state.tfstate"
     region         = "us-east-1"
     encrypt        = true
